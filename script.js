23d59fc75bc01825c444b761f17d630efe6982f3
Ananasaaa <kairyakn@gmail.com>
2024-08-22 01:01:21 +0300

    modified

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ class Calculator {
    }
 
    divide(num1, num2) {
-    return num1 / num2;
+    if(num2 === 0) {
+      return `Error!`
+    } else {
+      return num1 / num2;
+    }
    }
 
    }
